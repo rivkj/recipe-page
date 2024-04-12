2df6bd9d8577c1679eef504abffe7bda6e6565c4
rivkj <rivaskj07@gmail.com>
2024-04-12 17:48:50 -0400

    added function to retrieve json file information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+interface Recipe {
+  title: string,
+  image: string,
+  time: number,
+  description: string,
+  id: string
+}
 
-export default function Home() {
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch('http://localhost:4000/recipes')
+
+  return result.json()
+}
+
+export default async function Home() {
+  const recipes = await getRecipes()
+
   return (
     <main>
-       home
+       <div className="grid grid-cols-3 gap-8">
+        {recipes.map(recipe => (
+          
+        ))}
+       </div>
     </main>
   );
 }
