698f8efd4ec31be4e7abf933f637d04564348cc9
rivkj <rivaskj07@gmail.com>
2024-04-12 18:02:28 -0400

    Added cards to recipes for viewing and structured page - need to edit card screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 interface Recipe {
   title: string,
   image: string,
   time: number,
   description: string,
+  marination: string,
   id: string
 }
 
@@ -19,7 +29,21 @@ export default async function Home() {
     <main>
        <div className="grid grid-cols-3 gap-8">
         {recipes.map(recipe => (
-          
+          <Card key={recipe.id}>
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+                <CardContent>
+                  <p>{recipe.description}</p>
+                </CardContent>
+                <CardFooter>
+                  <button>View Recipe</button>
+                  {recipe.marination && <p>Marination time not added to actual recipe time!</p>}
+                </CardFooter>
+              </div>
+            </CardHeader>
+          </Card>
         ))}
        </div>
     </main>
