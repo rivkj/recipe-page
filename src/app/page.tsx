822eb97434f629d57985d48d56caf7547fa90f15
rivkj <rivaskj07@gmail.com>
2024-04-16 15:52:15 -0400

    fixed bug that was not retrieving new info to port, edited marination comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,34 +12,34 @@ interface Recipe {
   image: string,
   time: number,
   description: string,
-  marination: string,
-  id: string
+  marination: boolean,
+  id: string,
 }
 
 async function getRecipes(): Promise<Recipe[]> {
-  const result = await fetch('http://localhost:4000/recipes')
+  const result = await fetch('http://localhost:4001/recipes')
 
   return result.json()
 }
 
 export default async function Home() {
   const recipes = await getRecipes()
-
+  // console.log("Recipes array:", recipes);
   return (
     <main>
-       <div className="grid grid-cols-3 gap-8">
-        {recipes.map(recipe => (
-          <Card key={recipe.id}>
-            <CardHeader>
+       <div className="grid grid-cols-3 gap-4">
+       {recipes.map(recipe => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook.</CardDescription>
                 <CardContent>
                   <p>{recipe.description}</p>
                 </CardContent>
-                <CardFooter>
+                <CardFooter className="flex justify-between">
                   <button>View Recipe</button>
-                  {recipe.marination && <p>Marination time not added to actual recipe time!</p>}
+                  {recipe.marination && <p>Marination needed!</p>}
                 </CardFooter>
               </div>
             </CardHeader>
